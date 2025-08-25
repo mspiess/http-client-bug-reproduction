c6ac0c8b7f8d208561ca7e6736eeacc9fe395fa1
Brian Clozel <brian.clozel@broadcom.com>
2025-08-25 17:31:44 +0200

    Add wiretap debugging

--- src/main/kotlin/com/example/demo/DemoController.kt
+++ src/main/kotlin/com/example/demo/DemoController.kt
@@ -2,22 +2,25 @@ package com.example.demo
 
 import eu.rekawek.toxiproxy.ToxiproxyClient
 import eu.rekawek.toxiproxy.model.ToxicDirection
+import org.springframework.http.client.ReactorClientHttpRequestFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestClient
+import reactor.netty.http.client.HttpClient
 
 @RestController
 class DemoController {
 
-    private val restClient: RestClient = RestClient.create()
-
-    private val url: String = "http://toxiproxy"
-
-    private val toxiClient = ToxiproxyClient("toxiproxy", 8474)
+    private val restClient: RestClient = RestClient.builder()
+        .requestFactory(ReactorClientHttpRequestFactory(HttpClient.create().wiretap(true)))
+        .build()
 
     init {
-        val proxy = toxiClient.createProxy("target", "toxiproxy:80", "example.org:80")
-        proxy.toxics().limitData("limit", ToxicDirection.UPSTREAM, 500000)
+        val toxiClient = ToxiproxyClient("toxiproxy", 8474)
+        if (toxiClient.getProxyOrNull("target") == null) {
+            val proxy = toxiClient.createProxy("target", "toxiproxy:80", "example.org:80")
+            proxy.toxics().limitData("limit", ToxicDirection.UPSTREAM, 500000)
+        }
     }
 
     @GetMapping("/demo")
@@ -28,7 +31,7 @@ class DemoController {
 
         restClient
             .post()
-            .uri(url)
+            .uri("http://toxiproxy")
             .body(obj)
             .retrieve()
             .toEntity(Any::class.java)
